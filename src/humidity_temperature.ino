2b153b8cac2375e89d3897efa6e1b128029c4794
Clement Patout <clement.patout@gmail.com>
2016-09-11 22:02:02 +0200

    Update humidity_temperature init platformio

--- src/humidity_temperature.ino
+++ src/humidity_temperature.ino
@@ -4,10 +4,11 @@
  */
 
 #define MY_DEBUG
-#define MY_NODE_ID 104
+#define MY_NODE_ID 103
 #define HEART_BEAT_CYCLE 6   // 5min * 6 = every 30min
 #define BATTERY_REPORT_CYCLE 288 // 5min * 288 = every 24h
 #define SENSOR_RETRY 5
+#define RADIO_RETRY 5
 #define SLEEP_TIME 300000 // 5 * 60 * 1000 = 30000  sleep time between read (in milliseconds)
 
 #define MY_RADIO_NRF24
@@ -35,20 +36,33 @@
 
 int BATTERY_SENSE_PIN = A0;  // select the input pin for the battery sense point
 
-int batteryReportCounter = BATTERY_REPORT_CYCLE - 1;
-int heartBeatCounter= HEART_BEAT_CYCLE - 1;
-int sensorCounter = 0;
+int batteryReportCounter = BATTERY_REPORT_CYCLE ;
+int heartBeatCounter = HEART_BEAT_CYCLE;
 
 // DHT
 DHT dht(HUMIDITY_SENSOR_DIGITAL_PIN, DHT22);
 float lastTemp = -100;
 int lastHum = -100;
 int oldBatteryPcnt = 110;
-int oldTrippedValue = 0;
+int oldTrippedValue = true;
 
 MyMessage msgHum(CHILD_ID_HUM, V_HUM);
 MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
-MyMessage alive(CHILD_ID_TRIP, V_TRIPPED);
+MyMessage alive(CHILD_ID_TRIP, V_VAR1);
+
+void presentation()
+{
+  // Send the sketch version information to the gateway and Controller
+   sendSketchInfo("Humidity Battery", "1.5");
+
+    // Register all sensors to gw (they will be created as child devices)
+    present(CHILD_ID_HUM, S_HUM);
+    wait(100);
+    present(CHILD_ID_TEMP, S_TEMP);
+    wait(100);
+    present(CHILD_ID_TRIP, S_CUSTOM);
+    wait(100);
+}
 
 void setup()
 {
@@ -61,33 +75,28 @@ void setup()
   dht.begin();
 }
 
-void presentation()
-{
-  // Send the sketch version information to the gateway and Controller
-   sendSketchInfo("Humidity Battery", "1.4");
-
-    // Register all sensors to gw (they will be created as child devices)
-    present(CHILD_ID_HUM, S_HUM);
-    present(CHILD_ID_TEMP, S_TEMP);
-    present(CHILD_ID_TRIP, S_MOTION);
-}
-
 void loop()
 {
-  batteryReportCounter++;
-  heartBeatCounter++;
-
-  if (heartBeatCounter >= HEART_BEAT_CYCLE) {
-    int trippedValue = abs(oldTrippedValue - 1);
-    send(alive.set(trippedValue));
-    oldTrippedValue = trippedValue;
-    heartBeatCounter = 0;
+  bool aliveSent;
+  if (heartBeatCounter++ >= HEART_BEAT_CYCLE) {
+    #ifdef MY_DEBUG
+      Serial.print("Beat sent: ");
+      Serial.println(oldTrippedValue? 1 : 0);
+    #endif
+    int retry = 0;
+    while(!(aliveSent = send(alive.set(oldTrippedValue? 1 : 0))) && (retry++ < RADIO_RETRY)) {
+      wait(100);
+    }
+    if (aliveSent) {
+      oldTrippedValue = !oldTrippedValue;
+      heartBeatCounter = 0;
+    }
   }
 
   readSensor();
 
   // Check battery
-  if (batteryReportCounter >= BATTERY_REPORT_CYCLE) {
+  if (batteryReportCounter++ >= BATTERY_REPORT_CYCLE) {
     // get the battery Voltage
     int sensorValue = analogRead(BATTERY_SENSE_PIN);
     float batteryV  = sensorValue * VBAT_PER_BITS;
@@ -108,11 +117,13 @@ void loop()
         Serial.println("Battery percent sent: 0%");
       #endif
       sendBatteryLevel(0);
+      wait(100);
 
       batteryReportCounter = 0;
-    } else if (oldBatteryPcnt > batteryPcnt) {
+    } else {
       // Power up radio after sleep
       sendBatteryLevel(batteryPcnt);
+      wait(100);
       oldBatteryPcnt = batteryPcnt;
 
       batteryReportCounter = 0;
@@ -124,19 +135,24 @@ void loop()
 
 void readSensor()
 {
-  sensorCounter++;
-  wait(2000);
-  float temp = dht.readTemperature();
-  int hum = dht.readHumidity();
 
-  if (isnan(temp) | isnan(hum)) {
-    Serial.println("Failed reading from DHT");
-    if (sensorCounter - 1 < SENSOR_RETRY) {
-      Serial.print("RETRY ");
-      Serial.println(sensorCounter - 1);
-      readSensor();
+  float temp = NAN;
+  int hum = NAN;
+  int sensorCounter = 0;
+  while((isnan(temp) | isnan(hum)) && (sensorCounter++ < SENSOR_RETRY)) {
+    #ifdef MY_DEBUG
+      Serial.println("Reading...");
+    #endif
+    temp = dht.readTemperature();
+    hum = dht.readHumidity();
+    if (isnan(temp) | isnan(hum)) {
+      Serial.println("Failed reading from DHT");
+      if (sensorCounter < SENSOR_RETRY) {
+        Serial.print("RETRY ");
+        Serial.println(sensorCounter - 1);
+      }
     }
-   return;
+    wait(200);
   }
 
   #ifdef MY_DEBUG
@@ -154,21 +170,34 @@ void readSensor()
 
 void sendData(int hum, float temp)
 {
+  bool tempSent;
   float diffTemp = abs(lastTemp - temp);
   if (diffTemp > TEMP_TRANSMIT_THRESHOLD) {
-    send(msgTemp.set(temp, 1));
-    lastTemp = temp;
-    #ifdef MY_DEBUG
-      Serial.println('Temperature sent');
-    #endif
+    int retry = 0;
+    while(!(tempSent = send(msgTemp.set(temp, 1))) && (retry++ < RADIO_RETRY)) {
+      wait(100);
+    }
+    if (tempSent) {
+      lastTemp = temp;
+      #ifdef MY_DEBUG
+        Serial.println('Temperature sent');
+      #endif
+    }
   }
+
+  bool humSent;
   float diffHum = abs(lastHum - hum);
-  if (diffTemp > HUMI_TRANSMIT_THRESHOLD) {
-    send(msgHum.set(hum, 1));
-    lastHum = hum;
-    #ifdef MY_DEBUG
-      Serial.println('Humidity sent');
-    #endif
+  if (diffHum > HUMI_TRANSMIT_THRESHOLD) {
+    int retry = 0;
+    while(!(humSent = send(msgHum.set(hum, 1))) && (retry++ < RADIO_RETRY)) {
+      wait(100);
+    }
+    if (humSent) {
+      lastHum = hum;
+      #ifdef MY_DEBUG
+        Serial.println('Humidity sent');
+      #endif
+    }
   }
 
 }
